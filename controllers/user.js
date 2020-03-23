a86e6ad0110c8039d51fa8e3330c2d68b5f5b1f9
Coolbiker61 <coolbiker61@gmail.com>
2020-03-24 00:21:15 +0100

    ajout controller user

--- controllers/user.js
+++ controllers/user.js
@@ -1,25 +1,41 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/User');
+const models = require('../models');
 
 /* inscrit l'utilisateur si son email n'est pas déjà utilisé */
 exports.signup = (req, res, then) => {
+    let email = req.body.email;
+    let username = req.body.username;
+    let password = req.body.password;
+    let isAdmin = 0;
+
+    if (email == null || username == null || password == null) {
+        return res.status(400).json({ 'error': 'missing parameters' });
+    }
+    if (username.length >= 25 || username.length <= 4) {
+        return res.status(400).json({ 'error': 'wrong username length' });
+    }
+    if (!/^[a-zA-Z]\w{3,14}$/.test(password)) {
+        return res.status(400).json({ 'error': 'wrong password' });
+    }
+
     var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (regex.test(String(req.body.email).toLowerCase())) {
-        User.findOne({ email: req.body.email })
+    if (regex.test(String(email).toLowerCase())) {
+        models.User.findOne({ attributes: ['email'], where: { email: email } })
         .then(user => {
             if(!user) {
-                bcrypt.hash(req.body.password, 10)
+                bcrypt.hash(password, 10)
                 .then(hash => {
 
-                    var user = new User({
-                        email: req.body.email,
-                        password: hash
-                    });
-                    user.save()
-                    .then(() => {
-                        res.status(201).json({ message: 'Utilisateur créé !'})
+                    var newUser = models.User.create({
+                        email: email,
+                        username: username,
+                        password: hash,
+                        isAdmin: isAdmin
+                    })
+                    .then((newUser) => {
+                        res.status(201).json({ message: 'Utilisateur créé : '+newUser.id})
                     })
                     .catch(error => {
                         res.status(500).json({ error });
@@ -39,28 +55,36 @@ exports.signup = (req, res, then) => {
 
 /* connecte un utilisateur grace a son email et son mot de passe */
 exports.login = (req, res, then) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    if (email == null || password == null) {
+        return res.status(400).json({ 'error': 'missing parameters'});
+    }
+    
     var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (regex.test(String(req.body.email).toLowerCase())) {
-        User.findOne({ email: req.body.email.toLowerCase() })
+    if (regex.test(String(email).toLowerCase())) {
+        models.User.findOne({ where: { email: email } })
         .then(user => {
             if(!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !'});
+            } else {
+                bcrypt.compare(password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            return res.status(401).json({ error: 'Mot de passe incorrect !'});
+                        }
+                        res.status(200).json({
+                            userId: user._id,
+                            token: jwt.sign(
+                                { userId: user._id },
+                                'c19cf0ee354dee5163bf6b19f0a52cca1892ee15aa5acddd4df33d6a48d62075',
+                                { expiresIn: '24h' }
+                            )
+                        });
+                    })
+                    .catch(error => res.status(500).json({ error }));
             }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !'});
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'c19cf0ee354dee5163bf6b19f0a52cca1892ee15aa5acddd4df33d6a48d62075',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
     } else {
