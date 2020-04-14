628dacd562d9eae353d3a5bedec72ecf048c4a14
Coolbiker61 <coolbiker61@gmail.com>
2020-04-15 00:17:46 +0200

    quilljs modifier

--- frontend/quill.js
+++ frontend/quill.js
@@ -2,15 +2,31 @@
 var options = {
     debug: 'info',
     modules: {
-      //toolbar: {container: '#toolbar',}
+      toolbar: '#toolbar-container',
     },
     placeholder: 'Compose an epic...',
-    readOnly: true,
+    //readOnly: true,
     theme: 'snow'
 };
+let counter = 0;
+var editor;
+document.getElementById("repond").addEventListener("click", function (params) {
+    counter++;
+    var html = "<div id=\"editor"+counter+"\"><p>Hello World!</p></div>";
+    document.getElementById("repond").insertAdjacentHTML('afterend', html); 
+    editor = new Quill('#editor'+counter, options);
+})
+
+var quill = new Quill('#editor-container', {
+    modules: {
+      syntax: true,
+      toolbar: '#toolbar-container'
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'
+  });
 
 
-var editor = new Quill('#editor1', options);
 
 
 
